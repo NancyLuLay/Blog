87548996352c5695f0db0dbcef036eddf9534173
Nancy Lu <xnaaancy@hotmail.com>
2016-08-09 15:03:29 -0700

    all Blog files

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#home"
+  get "/home" => "home#home", as: :home
+
+  get "/about" => "home#about", as: :about
+end
